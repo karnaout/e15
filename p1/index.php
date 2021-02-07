7157008c1a6b4a05f68f1e18f4add08f779b3dc7
karnaout <karnaout@hsph.harvard.edu>
2021-02-07 09:26:02 -0500

    created functions templates

--- p1/index.php
+++ p1/index.php
@@ -1,3 +1,28 @@
 <?php
 
+/**
+ * Checks if a string is palindrome.
+ *
+ * @param string $str The string we want to check whether is a palindrome or not.
+ * 
+ * @return boolean
+ */
+function isPalindrome( $str ) 
+{
+    
+    return '';
+}
+
+/**
+ * Checks if a string is palindrome.
+ *
+ * @param string $str The string we want to check whether is a palindrome or not.
+ * 
+ * @return int
+ */
+function countVowels( $str ) 
+{
+    return 0;
+}
+
 require 'index-view.php';
