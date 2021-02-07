04bc8c3045f920fbf757d59a22cb5aedb9af54fc
karnaout <karnaout@hsph.harvard.edu>
2021-02-07 09:11:07 -0500

    created p1 template

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: *Khaled Arnaout*
++ Production URL: <http://e15p1.yourdomain.com>
+
+## Outside resources
+n/a
+
+## Notes for instructor
+n/a
---
+++ p1/index-view.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E15 Project 1</title>
+</head>
+<body>
+    <h1>E15 Project 1</h1>
+</body>
+</html>
