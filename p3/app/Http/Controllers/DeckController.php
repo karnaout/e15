85e4adac84577610fc5b9d6c343a814104daa7ed
karnaout <karnaout@hsph.harvard.edu>
2021-03-29 19:33:59 -0400

    created main controllers and routes

---
+++ p3/app/Http/Controllers/DeckController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DeckController extends Controller
+{
+    public function index(){
+        return 'Decks';
+    }
+}
--- p3/routes/web.php
+++ p3/routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\DeckController;
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\StudyController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Pages
+Route::get('/', [PageController::class, 'index']);
 
-Route::get('/', function () {
-    return view('/pages/home');
-});
+// Decks
+Route::get('/decks', [DeckController::class, 'index']);
+
+// Cards
+Route::get('/cards', [CardController::class, 'index']);
+
+// Study
+Route::get('/study', [StudyController::class, 'index']);
