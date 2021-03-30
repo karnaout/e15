30918f637c18a421ee42ebb92c40b6549bcad3f6
karnaout <karnaout@hsph.harvard.edu>
2021-03-30 09:35:55 -0400

    db migration

---
+++ p3/database/migrations/2021_03_30_125823_create_decks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDecksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('decks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            // slug VARCHAR
+            $table->string('slug');
+
+            // name VARCHAR
+            $table->string('name');
+
+            // description VARCHAR
+            $table->text('description')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('decks');
+    }
+}
---
+++ p3/database/migrations/2021_03_30_125836_create_cards_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            // slug VARCHAR
+            $table->string('slug');
+
+            // question VARCHAR
+            $table->string('question');
+
+            // answer VARCHAR
+            $table->text('answer');
+
+            // cover_url VARCHAR
+            $table->text('cover_url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
