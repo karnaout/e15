40bc795bf4c798b349d6da808d76a462d7772342
karnaout <karnaout@hsph.harvard.edu>
2021-04-01 09:17:46 -0400

    Study your cards page

--- p3/resources/views/study/index.blade.php
+++ p3/resources/views/study/index.blade.php
@@ -11,7 +11,26 @@ Study - FlashCards
 
 <h1>Study your cards</h1>
 <p class="lead">
-You can study your cards here...
+Select the game or exercise you would like to play.
 </p>
+<div class="row">
+    <div class="card col-md-5 position-relative m-3 pb-3">
+        <img class="card-img-top" src="images/memorize-cards.jpg" alt="Memorize cards">
+        <div class="card-body">
+            <h5 class="card-title text-dark">Memorize Cards</h5>
+            <p class="card-text text-dark">Memorize Cards uses a method known as  <strong>Spaced repetition</strong> to help you memorize your selected cards</p>
+            <a href="/memorize-cards" class="btn btn-primary position-absolute fixed-bottom">Play Memorize Cards</a>
+        </div>
+    </div>
+    <div class="card col-md-5 m-3 position-relative m-3 pb-3">
+        <img class="card-img-top" src="images/trivia.jpg" alt="Memorize cards">
+        <div class="card-body">
+            <h5 class="card-title text-dark">Trivia</h5>
+            <p class="card-text text-dark">Guess the name of the person, city or object in the photo</p>
+            <a href="/memorize-cards" class="btn btn-primary position-absolute fixed-bottom">Play Trivia</a>
+        </div>
+    </div>
+</div>
+
 
 @endsection
