c655aa5bdd255f84d406978955bba01ba2220aa9
karnaout <karnaout@hsph.harvard.edu>
2021-05-12 07:04:51 -0400

    register a new user test

--- p3/tests/Browser/AuthTest.php
+++ p3/tests/Browser/AuthTest.php
@@ -5,25 +5,29 @@ namespace Tests\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use Illuminate\Support\Str;
 
 class AuthTest extends DuskTestCase
 {
     /**
-     * A Dusk test for registration.
+     * A Dusk test for registering a new user.
      *
      * @return void
      */
     public function testRegistration()
     {
         $this->browse(function (Browser $browser) {
+            $rand_email = Str::random(5) . '@hotmail.com';
             $browser->visit('/register')
                     ->assertVisible('@register-heading')
-                    ->type('@name-input', 'Khaled Arnaout')
-                    ->type('@email-input', 'ka2@live.com')
+                    ->type('@name-input', 'Random User')
+                    ->type('@email-input', $rand_email )
                     ->type('@password-input', 'asdfasdf')
                     ->type('@confirm-password-input', 'asdfasdf')
-                    ->click('@register-button');
-                    // ->assertVisible('@welcome-heading');
+                    ->click('@register-button')
+                    ->assertVisible('@welcome-heading');
         });
     }
+
+
 }
